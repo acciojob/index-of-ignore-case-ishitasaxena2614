c231eb7200bdb904d72b1484c29440ddbe3844b4
ishitasaxena2614 <ishitasaxena324@gmail.com>
2025-11-05 22:45:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+  // Convert both strings to lowercase for case-insensitive comparison
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    // Return the index of the first occurrence of s2 in s1
+    // If not found, indexOf() automatically returns -1
+    return s1.indexOf(s2);
+} 
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
